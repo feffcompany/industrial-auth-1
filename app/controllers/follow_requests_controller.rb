26f8f3927df39a2ad28f557ab06c47c486dcf547
jhoffmeyer <jhoffmeyer@chicagobooth.edu>
2021-04-29 00:05:54 +0000

    Makes the project awesome

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -1,5 +1,6 @@
 class FollowRequestsController < ApplicationController
   before_action :set_follow_request, only: %i[ show edit update destroy ]
+  before_action :ensure_current_user_is_author, only:[:destroy, :update, :edit]
 
   # GET /follow_requests or /follow_requests.json
   def index
